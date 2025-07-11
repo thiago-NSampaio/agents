988412597cbd468d900a48138a6f22d388da5196
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2025-07-10 20:38:38 -0400

    docs: create rest client http file

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,26 @@
-import { fastifyCors } from "@fastify/cors";
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { env } from "./env.ts";
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: "http://localhost:5173",
+  origin: 'http://localhost:5173',
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.listen({ port: env.PORT }).then(() => {
-  app.log.info("HTTP server running!🚀");
+app.get('/health', () => {
+  return 'OK';
 });
+
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT });
